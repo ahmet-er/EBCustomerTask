780eb59e218abea29fb2e0e84b1b5400b9a29946
Ahmet Er <ahmet.er.14@gmail.com>
2024-08-02 16:26:44 +0300

    CustomerController

--- src/EBCustomerTask.WebUI/Controllers/CustomerController.cs
+++ src/EBCustomerTask.WebUI/Controllers/CustomerController.cs
@@ -1,12 +1,132 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EBCustomerTask.Application.DTOs;
+using EBCustomerTask.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EBCustomerTask.WebUI.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private readonly ICustomerService _customerService;
+        private readonly IIdentityService _identityService;
+
+        public CustomerController(ICustomerService customerService, IIdentityService identityService)
+        {
+            _customerService = customerService;
+            _identityService = identityService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _customerService.GetAllAsync();
+            return View(customers);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id is 0)
+            {
+                return NotFound();
+            }
+            
+            var customer = await _customerService.GetCustomerByIdAsync(id);
+
+            if (customer is null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CustomerCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _customerService.SaveCustomerAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id is 0)
+            {
+                return NotFound();
+            }
+
+            var customer = await _customerService.GetCustomerByIdAsync(id);
+            if (customer is null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, CustomerUpdateViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _customerService.UpdateCustomerAsync(model);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!CustomerExists(model.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id is 0)
+            {
+                return NotFound();
+            }
+
+            var customer = await _customerService.GetCustomerByIdAsync(id);
+            if (customer is null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _customerService.DeleteCustomerAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _customerService.GetCustomerByIdAsync(id) != null;
         }
     }
 }
